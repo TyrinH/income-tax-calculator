182bb632d234cb510df68bd0c292279023a7f19a
Tyrin Holloway <tychnk@gmail.com>
2022-08-13 22:56:32 -0500

    Created variables for the max tax rate for a given bracket

--- src/components/incomeTaxForm.vue
+++ src/components/incomeTaxForm.vue
@@ -8,12 +8,12 @@ function calcIncome() {
 
  // Example $50,000 for 2021
     //$0 to $9,950 = 10% THRESHOLD = $995
-    //$10,275 to $ 40,525 = 12% THRESHOLD = $4,863
-    //$40,525 to $89,075 = 22% THRESHOLD = $19,596.50
-    //$89,075 to $170,050 = 24% THRESHOLD = $40,812
-    //$170,050 to $215,950 = 32% THRESHOLD = $69,104
-    //$215,950 to $539,900 = 35% THRESHOLD = $188,956
-    //$539,900 or more = 37%
+    //$9,951 to $40,525 = 12% THRESHOLD = $4,863
+    //$40,526 to $86,375 = 22% THRESHOLD = $19,002.50
+    //$86,375 to $164,925 = 24% THRESHOLD = $39,582
+    //$164,926 to $209,425 = 32% THRESHOLD = $67,016
+    //$209,426 to $523,600 = 35% THRESHOLD = $183,260
+    //$523,601 or more = 37%
 
     // Example $50,000 for 2022
     //$0 to $10,275 = 10% THRESHOLD = $1,027.50
@@ -24,29 +24,49 @@ function calcIncome() {
     //$215,950 to $539,900 = 35% THRESHOLD = $188,956
     //$539,900 or more = 37%
 
+    const firstBracket2021 = 9950
+    const firstBrackerMaxTax2021 = 995
+    const secondBracket2021 = 40525
+    const secondBracketMaxTax2021 = 4863
+    const thirdBracket2021 = 86375
+    const thirdBracketMaxTax2021 = 19002.50
+    const fourthBracket2021 = 164925
+    const fourthBracketMaxTax2021 = 39582
+    const fifthBracket2021 = 209425
+    const fifthBracketMaxTax2021 = 67016
+    const sixthBracket2021 = 523600
+    const sixthBracketMaxTax2021 = 183260
+ 
+
     const firstBracket2022 = 10275
+    const firstBrackerMaxTax2022 = 1027.50
     const secondBracket2022 = 41775
+    const secondBracketMaxTax2022 = 5013
     const thirdBracket2022 = 89075
+    const thirdBracketMaxTax2022 = 19596.50
     const fourthBracket2022 = 170500
+    const fourthBracketMaxTax2022 = 40812
     const fifthBracket2022 = 215950
+    const fifthBracketMaxTax2022 = 69104
     const sixthBracket2022 = 539900
+    const sixthBracketMaxTax2022 = 188956
 
   if (income < 0) {
     // You didn't make money
     // Also handles errors for negative numbers
-  } else if (income >= 0 && income <= firstBracket2022) {
+  } else if (income >= 0 && income <= firstBracket2021) {
     console.log('You are in the first bracket')
-    if (income - firstBracket === 0) {
-        incomeTax = 1027.50
+    if (income - firstBracket2021 === 0) {
+        incomeTax = firstBrackerMaxTax2021
     }else {
         var incomeTax = income * 0.10;
     }
-  } else if (income > firstBracket2022 && income <= secondBracket2022){
+  } else if (income > firstBracket2021 && income <= secondBracket2021){
     console.log('You are in the second bracket')
-    if (income - secondBracket2022 === 0) {
-        incomeTax = 5013 + 1027.50
+    if (income - secondBracket2021 === 0) {
+        incomeTax = firstBrackerMaxTax2021 + secondBracketMaxTax2021
     } else {
-        var incomeTax = ((income - firstBracket2022) * 0.12) + 1027.50;
+        var incomeTax = ((income - firstBracket2021) * 0.12) + firstBrackerMaxTax2021;
     }
     //   var incomeTax = 1027.50 + ((income - 10275) * 0.12);
   } else if (income > 41775 && income < 89075) {
@@ -57,7 +77,7 @@ function calcIncome() {
       //Income >= 297350
       var incomeTax = 93374 + ((income - 297350) * 0.391);
   }
-    console.log('Your results:',incomeTax, income)
+    console.log('Your results:',incomeTax.toFixed(2), income)
 }
 </script>
 
