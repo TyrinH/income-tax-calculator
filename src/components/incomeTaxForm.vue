850d8447d9a0f43e03bc10ece8c3446374f553f1
Tyrin Holloway <tychnk@gmail.com>
2022-08-19 23:21:54 -0500

    Formatting for currency added, importing tax calculation to shorten file, lastly styled subtracted values in red and total take home in green

--- src/components/incomeTaxForm.vue
+++ src/components/incomeTaxForm.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { getFederalTaxAmount } from '../Helpers/federalTaxCalc.js'
+import { ref, computed } from 'vue'
+import { getFederalTaxAmount, getStateTaxAmount } from '../Helpers/federalTaxCalc.js'
 
 const text = ref('')
 const totalFederalTax = ref(0)
@@ -8,31 +8,12 @@ const totalSocialSecurityTax = ref(0)
 const totalMedicareTax = ref(0)
 const takeHomePay = ref(0)
 const totalStateTax = ref(0)
+const displayPay = ref('')
 function calcIncome () {
   const standardDeduction2021 = 12550
   const income = (parseInt(text.value) - standardDeduction2021)
   const incomeTax = getFederalTaxAmount(income)
-
-  let stateIncomeTax = 0
-  if (income > 0 && income <= 15000) {
-    if (income - 15000 === 0) {
-      stateIncomeTax = 465
-    } else {
-      stateIncomeTax = (income * 0.031)
-    }
-  } else if (income > 15000 && income <= 30000) {
-    if (income - 30000 === 0) {
-      stateIncomeTax = 930
-    } else {
-      stateIncomeTax = (income * 0.0525) + 465
-    }
-  } else if (income > 30000) {
-    if (income - 30000 === 0) {
-      stateIncomeTax = 1252.50
-    } else {
-      stateIncomeTax = (income * 0.057) + 1252.50
-    }
-  }
+  const stateIncomeTax = getStateTaxAmount(income)
 
   const socialSecurityTax = income * 0.062
   const medicareTax = income * 0.0145
@@ -40,21 +21,51 @@ function calcIncome () {
   totalSocialSecurityTax.value = socialSecurityTax
   totalMedicareTax.value = medicareTax
   totalStateTax.value = stateIncomeTax
-  takeHomePay.value = Math.round(((income - incomeTax - socialSecurityTax - medicareTax - stateIncomeTax) + standardDeduction2021))
+  takeHomePay.value = ((income - incomeTax - socialSecurityTax - medicareTax - stateIncomeTax) + standardDeduction2021)
+  displayPay.value = currencyFormatter.format(takeHomePay.value)
 }
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+})
+
+const filteredTakeHome = computed(() => {
+  return currencyFormatter.format(takeHomePay.value)
+})
+
+const filteredFederalTax = computed(() => {
+  return currencyFormatter.format(totalFederalTax.value)
+})
+
+const filteredTotalSocialSecurityTax = computed(() => {
+  return currencyFormatter.format(totalSocialSecurityTax.value)
+})
+
+const filteredTotalMedicareTax = computed(() => {
+  return currencyFormatter.format(totalMedicareTax.value)
+})
+
+const filteredTotalSalesTax = computed(() => {
+  return currencyFormatter.format(totalStateTax.value)
+})
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Income Tax Calculator</h1>
-    <h2>Your total federal tax amount is: ${{ totalFederalTax }}</h2>
-    <h2>Your total social security tax amount is: ${{ totalSocialSecurityTax }}</h2>
-    <h2>Your total mediacare tax amount is: ${{ totalMedicareTax }}</h2>
-    <h2>Your total kansas state tax amount is: ${{ totalStateTax }}</h2>
-    <h2>Your take home pay is: ${{ takeHomePay }}</h2>
+  <h1 class="text-3xl font-bold underline pb-4">Income Tax Calculator</h1>
+  <div class="grid grid-cols-4 gap-4">
+    <h2 class="">Your total federal tax amount is: <span class=" text-red-600"> {{ filteredFederalTax }}</span></h2>
+    <h2>Your total social security tax amount is: <span class=" text-red-600">{{ filteredTotalSocialSecurityTax }}</span></h2>
+    <h2>Your total mediacare tax amount is: <span class=" text-red-600">{{ filteredTotalMedicareTax }}</span></h2>
+    <h2>Your total kansas state tax amount is: <span class=" text-red-600">{{ filteredTotalSalesTax }}</span></h2>
   </div>
-    <form action="">
-        <input v-model="text" type="text" placeholder="Enter your income here">
-        <button type="button" @click.prevent="calcIncome">Submit</button>
+  <div>
+    <h2>Your take home pay is: <span class=" text-green-500 ">{{ filteredTakeHome }}</span></h2>
+  </div>
+  <div>
+      <form action="">
+        <input class=" pr-2" v-model="text" type="text" placeholder="Enter your income here">
+        <button class=" pl-4 pr-4 bg-blue-600" type="button" @click.prevent="calcIncome">Submit</button>
     </form>
+  </div>
 </template>
